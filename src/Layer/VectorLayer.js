722e31fb0bc13798d2f4ad432c392d74451b0216
fuzhen <fuzhen@maptalks.org>
2015-08-28 00:12:53 +0800

    commit VectorLayer

---
+++ src/Layer/VectorLayer.js
@@ -0,0 +1,34 @@
+Z.VectorLayer=Z.OverlayLayer.extend({
+
+    baseDomZIndex:200,
+
+    options:{
+        'render':'dom' // possible values: dom - svg or vml, canvas
+    },
+
+    /**
+     * 构造函数
+     * @param  {String} id 图层identifier
+     */
+    initialize:function(id, options) {
+        this.identifier = id;
+        Z.Util.setOptions(this, options);
+        //动态载入成员方法
+        if ('dom' === this.options['render'].toLowerCase()) {
+            Z.Util.extend(this, Z.Render.Dom);
+        } else if ('canvas' === this.options['render'].toLowerCase()) {
+            Z.Util.extend(this, Z.Render.Canvas);
+        }
+    },
+
+    /**
+     * 是否用Canvas渲染
+     * @return {Boolean}
+     * @expose
+     */
+    isCanvasRender:function() {
+        return 'canvas' === this.options['render'].toLowerCase();
+    }
+
+
+});
