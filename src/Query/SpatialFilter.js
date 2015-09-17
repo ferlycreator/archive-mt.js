8022604b55770b38a1b00eaf6ad50e7941cbde71
wangjun <wangjun@maptalks.org>
2015-09-17 15:47:28 +0800

    控件查询相关注释。

--- src/Query/SpatialFilter.js
+++ src/Query/SpatialFilter.js
@@ -1,21 +1,62 @@
+/**
+ * 空间过滤类
+ * @class maptalks.SpatialFilter
+ * @extends maptalks.Class
+ * @author Maptalks Team
+ */
 Z['SpatialFilter']=Z.SpatialFilter=Z.Class.extend({
+
     statics: {
+        /**
+         * @static
+         * @property {Number} RELATION_INTERSECT 相交
+         */
         'RELATION_INTERSECT' : 0,
+        /**
+         * @static
+         * @property {Number} RELATION_CONTAIN 包含
+         */
         'RELATION_CONTAIN' : 1,
+        /**
+         * @static
+         * @property {Number} RELATION_DISJOINT 分离
+         */
         'RELATION_DISJOINT' : 2,
+        /**
+         * @static
+         * @property {Number} RELATION_OVERLAP 重叠
+         */
         'RELATION_OVERLAP' : 3,
+        /**
+         * @static
+         * @property {Number} RELATION_TOUCH 接触
+         */
         'RELATION_TOUCH' : 4,
+        /**
+         * @static
+         * @property {Number} RELATION_WITHIN 在内部
+         */
         'RELATION_WITHIN' : 5,
+        /**
+         * @static
+         * @property {Number} RELATION_CONTAINCENTER 包含中心点
+         */
         'RELATION_CONTAINCENTER' : 7
     },
 
+    /**
+     * @constructor
+     * @param {maptalks.Geometry} geometry
+     * @param {Number} relation
+     */
     initialize:function(geometry, relation) {
         this.geometry = geometry;
         this.relation = relation;
     },
+
     /**
      * 获取SpatialFilter中的geometry
-     * @return {Geometry} SpatialFilter的Geometry
+     * @return {maptalks.Geometry} SpatialFilter的Geometry
      * @expose
      */
     getGeometry: function() {
