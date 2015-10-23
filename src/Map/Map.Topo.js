3fed675f2f380f4aaba232f8ec4155ec13cda177
fuzhen <fuzhen@maptalks.org>
2015-10-23 16:52:36 +0800

    identify查询无结果时, success仍然设为true

--- src/Map/Map.Topo.js
+++ src/Map/Map.Topo.js
@@ -79,7 +79,7 @@ Z.Map.include({
                 }
             }
         }
-        fn.call(this, {'success': hits.length > 0,'count':hits.length, 'data': hits});
+        fn.call(this, {'success': true, 'count':hits.length, 'data': hits});
     }
 
 });
