69914bf9d8cedd49bb654aa97e7cbe4a4b149ab0
fuzhen <fuzhen@maptalks.org>
2015-11-03 12:21:59 +0800

    add TileCache for TileLayer Renders

---
+++ src/Renders/TileLayer/TileCache.js
@@ -0,0 +1,37 @@
+Z.TileLayer.TileCache=function(capacity) {
+    this._queue = [];
+    this._cache = {};
+    if (!capacity) {
+        capacity = 1024;
+    }
+    this.capacity = capacity;
+};
+
+Z.TileLayer.TileCache.prototype = {
+    add:function(key, tile) {
+        this._cache[key] = tile;
+        this._queue.push(key);
+        this._expireCache();
+    },
+
+    get:function(key) {
+        return this._cache[key];
+    },
+
+    _expireCache:function() {
+        if (this._expTimeout) {
+            clearTimeout(this._expTimeout);
+        }
+        var me = this;
+        this._expTimeout = setTimeout(function() {
+            var len = me._queue.length;
+            if (len > me.capacity) {
+                var expir = me._queue.splice(0, len-me.capacity);
+                for (var i = expir.length - 1; i >= 0; i--) {
+                    delete me._cache[expir[i]];
+                }
+            }
+        },1000);
+
+    }
+};
