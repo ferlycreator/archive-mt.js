4cfae72bd0724e765a41a163389aa05fa25a2eb6
fuzhen <fuzhen@maptalks.org>
2015-11-06 14:37:08 +0800

    Geometry资源url由相对转化为绝对地址时, 增加对以"/"开头的相对地址的特殊处理, 解决 #40

--- src/Geometry/Geometry.js
+++ src/Geometry/Geometry.js
@@ -181,20 +181,24 @@ Z['Geometry']=Z.Geometry=Z.Class.extend({
             return true;
         }
         function absolute(base, relative) {
-            //FIXME 需要处理relative以'/'开头的情况
             var stack = base.split("/"),
                 parts = relative.split("/");
-            stack.pop(); // remove current file name (or empty string)
-                         // (omit if "base" is the current folder without trailing slash)
-            for (var i=0; i<parts.length; i++) {
-                if (parts[i] == ".")
-                    continue;
-                if (parts[i] == "..")
-                    stack.pop();
-                else
-                    stack.push(parts[i]);
+            if (relative.indexOf('/') === 0) {
+                return stack.slice(0,3).join('/')+relative;
+            } else {
+                stack.pop(); // remove current file name (or empty string)
+                             // (omit if "base" is the current folder without trailing slash)
+                for (var i=0; i<parts.length; i++) {
+                    if (parts[i] == ".")
+                        continue;
+                    if (parts[i] == "..")
+                        stack.pop();
+                    else
+                        stack.push(parts[i]);
+                }
+                return stack.join("/");
             }
-            return stack.join("/");
+
         }
 
         var icon = symbol['markerFile'];
