40ee9a11edef3a57c6326d8866b3f940f0d41958
fuzhen <fuzhen@maptalks.org>
2015-12-28 10:56:31 +0800

    restore UI/toolbar

--- src/Map/UI/Toolbar.js
+++ src/Map/UI/Toolbar.js
@@ -10,39 +10,49 @@ Z['Toolbar'] = Z.Toolbar = Z.Control.extend({
      * @cfg {Object} options 工具栏属性
      */
     options:{
-        "position" : {
-            "bottom": 0,
-            "right": 0
+        'position' : {
+            'bottom': '0',
+            'right': '0'
         },
-        "classname": "",
-        "vertical" : false,
-        "items": []
+        'classname': '',
+        'vertical' : false,
+        'items': [{
+            'type' : 'button',
+            'icon' : '',
+            'text': '左',
+            'click' : null,
+            'mouseover' : null,
+            'mouseout' : null
+        }, {
+            'type' : 'html',
+            'content':''
+        }]
     },
 
-    buildOn: function (map) {
+    _buildOn: function (map) {
         this._map = map;
-        var dom = Z.DomUtil.createEl('div');
-        var ul = Z.DomUtil.createEl('ul');
-        dom.appendChild(ul);
+        this._toolbarContainer = Z.DomUtil.createEl('div');
         if(this.options['className']) {
-            Z.DomUtil.addClass(dom, this.options['className']);
+            Z.DomUtil.addClass(this._toolbarContainer, this.options['className']);
         } else {
             if(this.options['vertical']) {
-                Z.DomUtil.addClass(dom, 'maptalks-toolbar-vertical');
+                Z.DomUtil.addClass(this._toolbarContainer, 'maptalks-control-toolbar-vertical');
             } else {
-                Z.DomUtil.addClass(dom, 'maptalks-toolbar-horizonal');
+                Z.DomUtil.addClass(this._toolbarContainer, 'maptalks-control-toolbar');
             }
         }
         var items = this.options['items'];
-        if(Z.Util.isArrayHasData(items)) {
+        if(items&&items.length>0) {
             for(var i=0,len=items.length;i<len;i++) {
                 var item = items[i];
                 if(!item['hidden']) {
+                    item['vertical'] = this.options['vertical'];
+                    item['position'] = this.options['position'];
                     var buttonDom = new Z.Button(item).getDom();
-                    ul.appendChild(buttonDom);
+                    this._toolbarContainer.appendChild(buttonDom);
                 }
             }
         }
-        return dom;
+        return this._toolbarContainer;
     }
 });
